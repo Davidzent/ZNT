e5b37e5245ffeecfb52e5ee205c7db4373827c73
davidznt <davidgin641@gmail.com>
2020-04-21 12:01:06 -0700

    Pushing discord

---
+++ VocalesToI/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: david
+ *
+ * Created on April 19, 2020, 10:13 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char name[15]="Message.txt";
+    ofstream out;
+    ifstream file;
+    file.open(name);
+    out.open("out.txt");
+    string s;
+    string sentence="";
+    while(file>>s){
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='u')s[i]='i';
+            else if(s[i]=='A'||s[i]=='E'||s[i]=='O'||s[i]=='U')s[i]='I';
+        }
+        sentence+=s+" ";
+    }
+    cout<<sentence;
+    out<<sentence;
+    file.close();
+    out.close();
+    return 0;
+}
+
--- WGUI/Minesweeper/Window.h
+++ WGUI/Minesweeper/Window.h
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   Window.h
@@ -17,7 +12,7 @@ class Window{
 public:
     void open(int,int);         //open a cell
     void mark(int,int);         //Flag or unflag a cell
-    void draw();        //Draws to the window
+    void draw();                //Draws to the window
 };
 
 
