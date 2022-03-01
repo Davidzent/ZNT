8e98509b76d96840a3a23d490472a533e3a93a8b
Davidgin <davidgin641@gmail.com>
2022-03-01 09:39:16 -0800

    Move Discord bot to here
    Signed-off-by: Davidgin <davidgin641@gmail.com>

--- Discord/Discord/Clan Manager/BOT/InstallLibraries.bat
+++
@@ -1,6 +0,0 @@
-call npm install discord.js@latest --save
-call npm install sequelize@latest --save
-call npm install mysql2@latest --save
-call npm install ffmpeg-static --save
-call npm install @discordjs/opus --save
-PAUSE
